5d3235aa25771b4dd0d1c901bd9be6342fa3de12
ubedsalto12 <65477300+ubedsalto12@users.noreply.github.com>
2022-11-14 23:23:53 +0800

    Delete paweb directory

--- paweb/koneksi.php
+++
@@ -1,6 +0,0 @@
-<?php
-$connection = mysqli_connect("localhost","root","","paweb");
-if(mysqli_connect_errno()){
-    echo "Koneksi database gagal : " . mysqli_connect_error();
-}
-?>
--- paweb/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-session_start();
-session_unset();
-session_destroy();
-
-header("Location: welcome.php");
-exit;
-?>
